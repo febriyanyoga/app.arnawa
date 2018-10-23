3e9d2a8d749700cd34e503108a78c98c425803bb
Galih Rahayu <galihrahayu@gmail.com>
2018-10-23 11:20:16 +0700

    layout tagihan

---
+++ application/views/TagihanV.php
@@ -0,0 +1,76 @@
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-body p-b-0">
+            <h4 class="card-title">Tagihan</h4><br>
+        <!-- Nav tabs -->
+        <ul class="nav nav-tabs customtab" role="tablist">
+            <li class="nav-item"> <a class="nav-link active" data-toggle="tab" href="#paid" role="tab"><span class="hidden-sm-up"><i class="fas fa-donate"></i></span> <span class="hidden-xs-down">Paid</span></a> </li>
+            <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#suspend" role="tab"><span class="hidden-sm-up"><i class="fas fa-ban"></i></span> <span class="hidden-xs-down">Suspend</span></a> </li>
+            <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#pending" role="tab"><span class="hidden-sm-up"><i class="fas fa-exclamation-triangle"></i></span> <span class="hidden-xs-down">Pending</span></a> </li>
+        </ul>
+        <!-- Tab panes -->
+        <div class="tab-content">
+            <div class="tab-pane active" id="paid" role="tabpanel">
+                <div class="p-20">
+                    <h4>Fitur anda saat ini</h4><br>
+                    <div class="table-responsive">
+                        <table id="multi_col_order" class="table table-striped table-bordered display" style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Fitur</th>
+                                    <th>Masa Aktif</th>
+                                    <th>Tanggal Mulai</th>
+                                    <th>Tanggal Berakhir</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Simpan Pinjam</td>
+                                    <td>3 Bulan</td>
+                                    <td>30 Januari</td>
+                                    <td>$112,000</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+            <div class="tab-pane  p-20" id="suspend" role="tabpanel">
+                <div class="p-20">
+                    <p>Pilih lanjutkan jika anda ingin melakukan perpanjangan masa aktif fitur.</p><br>
+                    <div class="table-responsive">
+                        <table id="multi_col_order" class="table table-striped table-bordered display" style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Fitur</th>
+                                    <th>Masa Aktif</th>
+                                    <th>Tanggal Mulai</th>
+                                    <th>Tanggal Berakhir</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Simpan Pinjam</td>
+                                    <td>3 Bulan</td>
+                                    <td>30 Januari</td>
+                                    <td>$112,000</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="tab-pane p-20" id="pending" role="tabpanel">3</div>
+        </div>
+        <div class="card-body p-t-0">
+            <pre class="language-html scrollable">
+                
+            </pre>
+        </div>
+    </div>
+</div>
