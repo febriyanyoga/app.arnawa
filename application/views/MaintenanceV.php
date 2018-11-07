bc73a7c8ad67ef4418f320ef05a4fd47cf1f4dda
Galih Rahayu <galihrahayu@gmail.com>
2018-11-07 14:21:54 +0700

    maintenance

---
+++ application/views/MaintenanceV.php
@@ -0,0 +1,17 @@
+
+
+<!-- ============================================================== -->
+<!-- MULAI : DATA TABEL -->
+<!-- ============================================================== -->
+
+<div class="error-box" style="width: 85%">
+    <div class="error-body text-center">
+        <h1 class="error-title" style="font-size: 100px;">Maintenance</h1>
+        <h3 class="text-uppercase error-subtitle">Silahkan Coba Lagi Nanti !</h3>
+        <p class="text-muted m-t-30 m-b-30">Halaman ini sedang mengalami maintenance untuk beberapa saat</p>
+        <a href="<?php echo site_url()?>KoperasiC/dashboard" class="btn btn-info btn-rounded waves-effect waves-light m-b-40">Back to home</a> 
+    </div>
+</div>
+<!-- ============================================================== -->
+<!-- END : DATA TABEL -->
+<!-- ============================================================== -->
